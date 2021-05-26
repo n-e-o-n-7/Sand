f731f1d88c12a46810ef8f864e8a2366ad1e60f4
n-e-o-n-7 <44162454+n-e-o-n-7@users.noreply.github.com>
2021-05-26 23:25:59 +0800

    M contentjs

--- SandWebExtension/Resources/content.js
+++ SandWebExtension/Resources/content.js
@@ -6,43 +6,98 @@ function getConfig(){
         config = response.config || config
     })
 }
+
 browser.storage.onChanged.addListener((changes,area)=>{
     config = changes["config"].newValue
-//    let changedItems = Object.keys(changes);
-//    for (let item of changedItems) {
-//      console.log(item + " has changed:");
-//      console.log("Old value: ");
-//      console.log(changes[item].oldValue);
-//      console.log("New value: ");
-//      console.log(changes[item].newValue);
-//    }
 })
+
 document.addEventListener("DOMContentLoaded", getConfig);
 
 //query
-document.onmousedown = function(event) {
+document.onmousedown = (event) => {
     if (event.button === 0){
-        document.addEventListener("mouseup", up);
+        let container = document.getElementById("sandContainer")
+        if (container.style.visibility === "hidden"){
+            document.addEventListener("mouseup", up);
+        }else {
+            container.style.visibility = "hidden"
+        }
     }
 }
 
 function up(event) {
     window.removeEventListener("mouseup", up);
-    
+
     let content = window.getSelection().toString()
     content = content.replace(/^\s+|\s+$/g, "");
-    
+
     if (content != "") {
-        let pos = {x:event.clickX,y:event.clickY}
+        let pos = {x:event.pageX,y:event.pageY}
         browser.runtime.sendMessage({ type:"query",content })
         .then((response) => {
             if(response){
-                console.log(JSON.parse(response))
+                let text = JSON.parse(response)
+                console.log(text)
+                draw(text.translation,pos)
             }
         });
     }
 }
 
-window.onresize = function(event){
-    
+//document.addEventListener("click",(e)=>{
+//    let range;
+//    let textNode;
+//    let offset;
+//
+//    if (document.caretPositionFromPoint) {
+//        range = document.caretPositionFromPoint(e.clientX, e.clientY);
+//        textNode = range.offsetNode;
+//        offset = range.offset;
+//
+//    } else if (document.caretRangeFromPoint) {
+//        range = document.caretRangeFromPoint(e.clientX, e.clientY);
+//        textNode = range.startContainer;
+//        offset = range.startOffset;
+//    }
+//
+//    let content = window.getSelection().toString()
+//
+//    if (textNode && textNode.nodeType == 3&&content) {
+//        var replacement = textNode.splitText(offset);
+//    }
+//},false)
+
+//draw
+function drawcontainer(){
+    let container = document.createElement('div');
+    container.id = "sandContainer"
+    container.style.background = "white"
+    container.style.padding = "10px"
+    container.style.zIndex = "1000"
+    container.style.position = "absolute"
+    container.style.left = "0px"
+    container.style.top = "0px"
+    container.style.visibility = "hidden"
+    let textNode = document.createTextNode("");
+    container.appendChild(textNode)
+    container.addEventListener("mousedown",(event)=>{
+        event.stopPropagation()
+    })
+    document.body.insertBefore(container,document.body.firstChild)
+}
+
+document.body.onload = drawcontainer ;
+
+function draw(text,pos){
+    console.log(pos)
+    let container = document.getElementById("sandContainer")
+    container.firstChild.textContent = text
+    container.style.left = pos.x + "px"
+    container.style.top = pos.y + "px"
+    container.style.visibility = "visible"
+}
+
+window.onresize = () => {
+    let container = document.getElementById("sandContainer")
+    container.style.visibility = "hidden"
 }
--- SandWebExtension/SafariWebExtensionHandler.swift
+++ SandWebExtension/SafariWebExtensionHandler.swift
@@ -5,31 +5,29 @@
 //  Created by 许滨麟 on 2021/5/23.
 //
 
-import SafariServices
 import os.log
+import SafariServices
 
 let SFExtensionMessageKey = "message"
 
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
-
-	func beginRequest(with context: NSExtensionContext) {
+    func beginRequest(with context: NSExtensionContext) {
         let item = context.inputItems[0] as! NSExtensionItem
         let message = item.userInfo?[SFExtensionMessageKey]
         os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
 
-      
-        var result :String? = nil
-        if let content = message as? String,let word = Stardict.query(word: content),let data = try? JSONEncoder().encode(word){
-            result = String(data:data, encoding: .utf8)
+        var result: String?
+        if let content = message as? String, let word = Stardict.query(word: content), let data = try? JSONEncoder().encode(word) {
+            result = String(data: data, encoding: .utf8)
         }
-        
+
         let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: result ?? ""]
+        response.userInfo = [SFExtensionMessageKey: result ?? ""]
         context.completeRequest(returningItems: [response], completionHandler: nil)
     }
 }
 
-//func NativeAPPToHandler(){
+// func NativeAPPToHandler(){
 //    NSUserDefaults - App groups
 //    NSXPCConnection
-//}
+// }
